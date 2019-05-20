a14ea8ed755e60f4f6af685d6773e4d464eb6e3b
az2009 <jefferson.porto@ymail.com>
2019-05-20 10:16:28 -0300

    ON-13282 - setting to when storeId is zero

--- src/app/code/community/BSeller/SkyHub/Model/Adminhtml/Sales/Order/Create.php
+++ src/app/code/community/BSeller/SkyHub/Model/Adminhtml/Sales/Order/Create.php
@@ -218,4 +218,70 @@ class BSeller_SkyHub_Model_Adminhtml_Sales_Order_Create extends Mage_Adminhtml_M
         
         return !empty($result);
     }
+
+    /**
+     * Validate quote data before order creation
+     *
+     * @return Mage_Adminhtml_Model_Sales_Order_Create
+     */
+    protected function _validate()
+    {
+        $customerId = $this->getSession()->getCustomerId();
+        if (is_null($customerId)) {
+            Mage::throwException(Mage::helper('adminhtml')->__('Please select a customer.'));
+        }
+
+        $storeId = $this->getSession()->getStore()->getId();
+        if (!$storeId && $storeId != 0) {
+            Mage::throwException(
+                Mage::helper('adminhtml')->__('Please select a store. %s', var_export($storeId, true))
+            );
+        }
+
+        $items = $this->getQuote()->getAllItems();
+
+        if (count($items) == 0) {
+            $this->_errors[] = Mage::helper('adminhtml')->__('You need to specify order items.');
+        }
+
+        foreach ($items as $item) {
+            $messages = $item->getMessage(false);
+            if ($item->getHasError() && is_array($messages) && !empty($messages)) {
+                $this->_errors = array_merge($this->_errors, $messages);
+            }
+        }
+
+        if (!$this->getQuote()->isVirtual()) {
+            if (!$this->getQuote()->getShippingAddress()->getShippingMethod()) {
+                $this->_errors[] = Mage::helper('adminhtml')->__('Shipping method must be specified.');
+            }
+        }
+
+        if (!$this->getQuote()->getPayment()->getMethod()) {
+            $this->_errors[] = Mage::helper('adminhtml')->__('Payment method must be specified.');
+        } else {
+            $method = $this->getQuote()->getPayment()->getMethodInstance();
+            if (!$method) {
+                $this->_errors[] = Mage::helper('adminhtml')->__('Payment method instance is not available.');
+            } else {
+                if (!$method->isAvailable($this->getQuote())) {
+                    $this->_errors[] = Mage::helper('adminhtml')->__('Payment method is not available.');
+                } else {
+                    try {
+                        $method->validate();
+                    } catch (Mage_Core_Exception $e) {
+                        $this->_errors[] = $e->getMessage();
+                    }
+                }
+            }
+        }
+
+        if (!empty($this->_errors)) {
+            foreach ($this->_errors as $error) {
+                $this->getSession()->addError($error);
+            }
+            Mage::throwException('');
+        }
+        return $this;
+    }
 }
